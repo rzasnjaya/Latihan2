2672fdfc35fe351eb80bc685c26f869a977305f8
rzasnjaya <sanjayareza77@gmail.com>
2026-03-27 12:09:17 +0700

    Basic Hit Effects

---
+++ Assets/RailShooter/Scripts/HitEffects.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffects : MonoBehaviour, IHitable
+{
+    [SerializeField] GameObject effectsPrefabs;
+
+    private ParticleSystem effectsCache;
+
+    public void Hit(RaycastHit hit)
+    {
+        if (effectsCache != null)
+        {
+            effectsCache.transform.position = hit.point;
+            effectsCache.transform.rotation = Quaternion.LookRotation(hit.normal);
+            effectsCache.Play();
+        }
+    }
+
+    void Start()
+    {
+        if (effectsPrefabs != null)
+        {
+            GameObject effectsTemp = Instantiate(effectsPrefabs, transform);
+            effectsCache = effectsTemp.GetComponent<ParticleSystem>();
+        }
+    }
+}
