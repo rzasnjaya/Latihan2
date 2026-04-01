8f9860a6dc5111ae32f2fab7f8ca6da4f2912412
rzasnjaya <sanjayareza77@gmail.com>
2026-04-01 16:33:05 +0700

    Auto Shoot

---
+++ Assets/Shootem Up/Scripts/Weapons/AutoShoot.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoShoot : MonoBehaviour
+{
+    public ShootProfile shootProfile;
+    public GameObject bulletPrefabs;
+    public Transform firePoint;
+
+    private float totalSpread;
+    private WaitForSeconds rate, interval;
+
+    void OnEnable()
+    {
+        interval = new WaitForSeconds(shootProfile.interval);
+        rate = new WaitForSeconds(shootProfile.fireRate);
+
+        if (firePoint == null)
+            firePoint = transform;
+
+        totalSpread = shootProfile.spread * shootProfile.amount;
+
+            StartCoroutine(ShootingSequence());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator ShootingSequence()
+    {
+        yield return rate;
+
+        while (true)
+        {
+            float angle = 0f;
+
+            if (shootProfile.amount > 1)
+            {
+                for (int i = 0; i < shootProfile.amount; i++)
+                {
+                    angle = totalSpread * (i / (float)shootProfile.amount);
+                    angle -= (totalSpread / 2f) - (shootProfile.spread / shootProfile.amount);
+
+                    Shoot(angle);
+
+                    if (shootProfile.fireRate > 0f)
+                        yield return rate;
+                }
+            }
+
+            yield return interval;
+        }
+    }
+
+    void Shoot(float angle)
+    {
+        GameObject temp = PoolingManager.instance.UseObject(bulletPrefabs, firePoint.position, firePoint.rotation);
+        temp.transform.Rotate(Vector3.up, angle);
+    }
+}
