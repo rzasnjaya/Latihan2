5fa297af776c5b54e3b7884462b4dc1399d3f922
rzasnjaya <sanjayareza77@gmail.com>
2026-04-02 15:01:15 +0700

    Node Move Moving Objects

--- Assets/Shootem Up/Scripts/Transform/NodeMove.cs
+++ Assets/Shootem Up/Scripts/Transform/NodeMove.cs
@@ -4,9 +4,58 @@ using UnityEngine;
 
 public class NodeMove : MonoBehaviour
 {
+    public float speed = 2f, rotateSpeed = 2f, bankingValue = 5f;
+    public bool rotateObject, loopMove;
+    public int loopToNode;
     public List<Vector3> nodes = new List<Vector3>();
 
     private const int CURVE_SEGMENT = 20;
+    private int realLoopNode;
+
+    private void OnEnable()
+    {
+        StartCoroutine(StartMove());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator StartMove()
+    {
+        int posID = 0;
+        List<Vector3> path = GetCurveNodes();
+
+        while (loopMove || posID < path.Count - 1)
+        {
+            if ((path[posID] - transform.position).sqrMagnitude < 0.01f)
+            {
+                if (loopMove)
+                {
+                    if (posID < path.Count - 1)
+                    {
+                        posID += 1;
+                    }
+                    else
+                    {
+                        posID = realLoopNode;
+                    }
+                }
+                else
+                {
+                    if (posID < path.Count - 1)
+                    {
+                        posID += 1;
+                    }
+                }
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, path[posID], speed * Time.deltaTime);
+
+            yield return null;
+        }
+    }
 
     List<Vector3> GetCurveNodes()
     {
@@ -22,6 +71,7 @@ public class NodeMove : MonoBehaviour
 
             if (i == 0)
             {
+                p0 = transform.position;
                 curvedNodes.Add(CalculateBezierPath(p0, p1, p2, p3, 0f));
             }
 
@@ -31,6 +81,8 @@ public class NodeMove : MonoBehaviour
                 curvedNodes.Add(CalculateBezierPath(p0, p1, p2, p3, t));
             }
         }
+
+        realLoopNode = (int)(curvedNodes.Count * (loopToNode / (float)nodes.Count));
 
         return curvedNodes;
     }
