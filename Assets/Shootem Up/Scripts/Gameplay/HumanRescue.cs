8250d4a6b6b9f7904f0155e044c9c68e0acf61c7
rzasnjaya <sanjayareza77@gmail.com>
2026-04-06 14:44:31 +0700

    Human Rescue

---
+++ Assets/Shootem Up/Scripts/Gameplay/HumanRescue.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HumanRescue : MonoBehaviour
+{
+    public float rescueTime = 5f;
+    public Image timerUI;
+    public UnityEvent onRescued;
+
+    IEnumerator Rescuing(float time)
+    {
+        while(time > 0f)
+        {
+            time -= Time.deltaTime;
+            UpdateUI(time);
+            yield return null;
+        }
+
+        onRescued.Invoke();
+        Destroy(gameObject, 1.5f);
+    }
+
+    void UpdateUI(float time)
+    {
+        if (timerUI != null)
+        {
+            float value = 1f - (time / rescueTime);
+            timerUI.fillAmount = value;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Rescuing(rescueTime));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+            timerUI.fillAmount = 0f;
+        }
+    }
+}
