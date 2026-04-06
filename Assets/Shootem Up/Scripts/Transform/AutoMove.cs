2e408b4040fd4cbfd0bb6022f575891c971fdae9
rzasnjaya <sanjayareza77@gmail.com>
2026-04-06 11:03:22 +0700

    World Moving

---
+++ Assets/Shootem Up/Scripts/Transform/AutoMove.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutoMove : MonoBehaviour
+{
+    public Vector3 moveOffset;
+    public bool onStart, reverse;
+    public float duration;
+    public UnityEvent onStartMove, onMoveDone;
+
+    private Vector3 targetPos, initialPos;
+    private float moveDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.localPosition;
+        moveDistance = moveOffset.magnitude;
+
+        if (onStart)
+        {
+            Move(reverse);
+        }
+    }
+
+    public void Move(bool reverse)
+    {
+        StartCoroutine(StartMove(reverse, duration));
+    }
+
+    IEnumerator StartMove(bool reverse, float time)
+    {
+        if (reverse)
+        {
+            targetPos = initialPos;
+            transform.localPosition += moveOffset;
+        }
+        else
+            targetPos = transform.localPosition + moveOffset;
+
+        onStartMove.Invoke();
+
+        while (transform.localPosition != targetPos)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos,
+                (moveDistance / time) * Time.deltaTime);
+
+            yield return null;
+        }
+
+        onMoveDone.Invoke();
+    }
+}
