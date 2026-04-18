c53fd5a494dd93b7bfd9920df79c5f3e00a57390
rzasnjaya <sanjayareza77@gmail.com>
2026-04-18 10:25:16 +0700

    Setting Up a stock box

---
+++ Assets/StoreGame/Scripts/StockBoxController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockBoxController : MonoBehaviour
+{
+    public List<Transform> bigDrinkPoints;
+    public List<Transform> cerealPoints, tubeChipsPoints, fruitPoints, largeFruitPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
