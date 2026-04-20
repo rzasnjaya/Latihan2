9af046a55cdd50135f77a10bbc4529094c0ae995
rzasnjaya <sanjayareza77@gmail.com>
2026-04-20 11:02:51 +0700

    Setting Up Money Tracking

---
+++ Assets/StoreGame/Scripts/StoreController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StoreController : MonoBehaviour
+{
+    public static StoreController instance;
+
+    public void Awake()
+    {
+        instance = this;        
+    }
+
+    public float currentMoney = 1000f;
+
+    public Transform stockSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.iKey.wasPressedThisFrame)
+        {
+            AddMoney(100f);
+        }
+
+        if (Keyboard.current.oKey.wasPressedThisFrame)
+        {
+            if (CheckMoneyAvailable(250f))
+            {
+                SpendMoney(250f);
+            }
+        }
+    }
+
+    public void AddMoney(float amountToAdd)
+    {
+        currentMoney += amountToAdd;
+    }
+
+    public void SpendMoney(float amountToSpend)
+    {
+        currentMoney -= amountToSpend;
+
+        if (currentMoney < 0)
+        {
+            currentMoney = 0;
+        }
+    }
+
+    public bool CheckMoneyAvailable(float amountToCheck)
+    {
+        bool hasEnough = false;
+
+        if (currentMoney >= amountToCheck)
+        {
+            hasEnough = true;
+        }
+
+        return hasEnough;
+    }
+}
