78c245f8d26020ae2093144b0ef9489a8ff1382b
rzasnjaya <sanjayareza77@gmail.com>
2026-04-21 14:54:06 +0700

    Moving to points and waiting

--- Assets/StoreGame/Scripts/Customer.cs
+++ Assets/StoreGame/Scripts/Customer.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
+    public List<Transform> points = new List<Transform>();
+
+    public float moveSpeed;
+    private float currentWaitTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentWaitTime = .5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (points.Count > 0)
+        {
+            MoveToPoint();
+        }
+    }
+
+    public void MoveToPoint()
+    {
+        Vector3 targetPosition = new Vector3(points[0].position.x, transform.position.y, points[0].position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        transform.LookAt(targetPosition);
+
+        if (Vector3.Distance(transform.position, targetPosition) < .25f)
+        {
+            if (currentWaitTime > 0)
+            {
+                currentWaitTime -= Time.deltaTime;  
+
+                if (currentWaitTime <= 0)
+                {
+                    StartNextPoint();
+                }
+            }
+        }
+    }
+
+    public void StartNextPoint()
+    {
+        if (points.Count > 0)
+        {
+            points.RemoveAt(0);
+
+            if (points.Count > 0)
+            {
+                currentWaitTime = .5f;
+            }
+        }
     }
 }
